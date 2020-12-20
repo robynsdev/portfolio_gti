7735f8c79651a4f3359cd08c869bf760b9f4f9af
robyn <65057146+robynsdev@users.noreply.github.com>
2020-12-20 23:08:49 +1100

    js hamburger btn and styling

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ console.log(burger.classList == "fas fa-bars");
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   menu.classList.toggle("active-menu");
-  if (burger.classList == "active") {
-    burger.classList.remove("fa-bars");
-    burger.classList.add("fa-times");
-  } else {
-    burger.classList.remove("fa-times");
-    burger.classList.add("fa-bars");
-  }
+  // if (burger.classList == "active") {
+  //   burger.classList.remove("fa-bars");
+  //   burger.classList.add("fa-times");
+  // } else {
+  //   burger.classList.remove("fa-times");
+  //   burger.classList.add("fa-bars");
+  // }
 });
 
 // fas fa-times
